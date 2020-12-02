fc8fd3a11555c7d658279da0f55d1f06407bb898
Juan Giovanni Restrepo <grestrepojimenez@gmail.com>
2020-12-01 20:41:20 -0500

    Componente producto pasado a clases para explicar ciclo de vida

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,16 +1,54 @@
-const Product = () => {
-    return (
-        <div className='col-4' >
-            <div>
-                <img src="https://www.pequerecetas.com/wp-content/uploads/2019/10/arepas-receta-768x523.jpg" class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <h5 class="card-title">Arepas</h5>
-                    <p class="card-text">Deliciosas arepas de bello.</p>
-                    <a href="#" class="btn btn-primary">Comprar</a>
+import React, { Component } from 'react'
+
+
+class Product extends Component {
+
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            "title": "Arepa"
+        }
+        console.log('hola soy el constructor')
+        this.cambiarTitulo = this.cambiarTitulo.bind(this)
+    }
+
+    componentDidMount() {
+        console.log('hola el componente producto fue renderizado tambien llamado "montado"')
+
+    }
+
+    componentDidUpdate() {
+        console.log('el componente fue actualizado')
+    }
+
+    componentWillUnmount(){
+        console.log('el componente será desmontado')
+    }
+
+    cambiarTitulo() {
+        this.setState({
+            "title": "Arepa de chocolo"
+        })
+    }
+
+    render() {
+        console.log('hola soy el render')
+        return (
+            <div className='col-4' >
+                <div>
+                    <img src="https://www.pequerecetas.com/wp-content/uploads/2019/10/arepas-receta-768x523.jpg"
+                        className="card-img-top"
+                        alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{this.state.title}</h5>
+                        <p className="card-text">Deliciosas arepas de bello.</p>
+                        <button  onClick={this.cambiarTitulo} className="btn btn-primary">Comprar</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 
 }
 export default Product
